2047ccadd76bc684b6c1eecb850092cc8ef39133
abbsamoze <69853489+abbsamoze@users.noreply.github.com>
2021-12-17 12:13:03 +0100

    fixed submit error

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,30 +1,4 @@
 <template>
-  <!-- <q-page class="flex flex-center"> -->
-    <!-- <div class="flex flex-center">
-      <h1 style="font-size:30px; color: white;">Hur pigg känner du dig idag?</h1>
-      <q-btn-toggle
-        style=""
-        v-model="model"
-        color="purple"
-        text-color="white"
-        toggle-color="orange"
-        toggle-text-color="black"
-        rounded
-        unelevated
-        glossy
-        :options="[
-          {label: '1', value: 'one', id:1},
-          {label: '2', value: 'two', id:2},
-          {label: '3', value: 'three', id:3},
-          {label: '4', value: 'four', id:4},
-          {label: '5', value: 'five', id:5}
-        ]"
-      />
-      <q-btn color="secondary" icon-right="mail" label="Submit Answer" v-on:click="sumbit()" style="margin-top:20vw;"/>
-    </div> -->
-    
-  <!-- </q-page> -->
-
  <div class="q-pa-md">
     <q-form @submit="onSubmit" class="q-gutter-md">
       <div class="q-mt-xl">
@@ -39,11 +13,11 @@
         />
 
         <h1 id="title">Hur många timmar har du sovit idag?</h1>
-        <q-radio dark name="Sömntid" v-model="Sömntid" val="0" label="1" id="radio" @click="submitTrue()"/>
-        <q-radio dark name="Sömntid" v-model="Sömntid" val="1-3" label="2-3" id="radio" @click="submitTrue()"/>
-        <q-radio dark name="Sömntid" v-model="Sömntid" val="4-6" label="4-6" id="radio" @click="submitTrue()"/>
-        <q-radio dark name="Sömntid" v-model="Sömntid" val="7-9" label="7-9" id="radio" @click="submitTrue()"/>
-        <q-radio dark name="Sömntid" v-model="Sömntid" val="10+" label="10+" id="radio" @click="submitTrue()"/>
+        <q-radio dark name="Sömntid" v-model="Sömntid" val="0" label="1" id="radio" />
+        <q-radio dark name="Sömntid" v-model="Sömntid" val="1-3" label="2-3" id="radio" />
+        <q-radio dark name="Sömntid" v-model="Sömntid" val="4-6" label="4-6" id="radio" />
+        <q-radio dark name="Sömntid" v-model="Sömntid" val="7-9" label="7-9" id="radio" />
+        <q-radio dark name="Sömntid" v-model="Sömntid" val="10+" label="10+" id="radio" />
       </div>
 
       <div>
@@ -80,7 +54,6 @@ export default {
       submitResult,
 
       onSubmit (evt) {
-        if(s) {
           const formData = new FormData(evt.target)
           const data = []
 
@@ -108,16 +81,11 @@ export default {
               "Content-Type": "application/x-www-form-urlencoded",
             },
           })
-        }
       }
     }
   },
   methods: {
-    submitTrue() {
-      console.log(s);
-      s = true;
-      console.log(s);
-    }
+
   }
 }
 </script>
